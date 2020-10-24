19779bc172b6dfdc274741b6204ca9ada7aa0b44
Liu <lc-soft@live.cn>
2020-10-24 12:03:55 +0800

    build(scripts): use `cmake --build` command to build project
    close lc-soft/LCUI#228

--- scripts/lib/cmake.js
+++ scripts/lib/cmake.js
@@ -1,5 +1,4 @@
 const { spawnSync } = require('child_process')
-const msbuild = require('./msbuild')
 
 module.exports = {
   name: 'cmake',
@@ -13,16 +12,7 @@ module.exports = {
     return spawnSync('cmake', params, { stdio: 'inherit', cwd: options.buildDir })
   },
   build(options) {
-    if (options.platform === 'windows') {
-      if (!msbuild.exists()) {
-        throw new Error('Visual Studio not found, build stopped.')
-      }
-      if (options.mode === 'debug') {
-        return msbuild.exec('..\\scripts\\build-bin-x64-dbg', { cwd: options.buildDir })
-      }
-      return msbuild.exec('..\\scripts\\build-bin-x64-rel', { cwd: options.buildDir })
-    }
-    return spawnSync('make', { stdio: 'inherit', cwd: options.buildDir })
+    return spawnSync('cmake', ['--build', '.'], { stdio: 'inherit', cwd: options.buildDir })
   },
   run(options) {
     return spawnSync(options.targetPath, { stdio: 'inherit', cwd: options.targetDir })
