767c151ad095f41ad58b4dce735d0bc413b385f9
Liu <lc-soft@live.cn>
2023-12-13 23:04:53 +0800

    build: upgrade lcui/react

--- src/components/about.tsx.h
+++ src/components/about.tsx.h
@@ -15,19 +15,19 @@ static void about_init_prototype(void)
         about_proto = ui_create_widget_prototype("About", NULL);
 }
 
-/** UTF-8 encoded string from: lcui-quick-start */
+// UTF-8 encoded string from: lcui-quick-start
 static const unsigned char widget_text_0[] = {0x6c, 0x63, 0x75, 0x69, 0x2d, 0x71, 0x75, 0x69, 0x63, 0x6b, 0x2d, 0x73, 0x74, 0x61, 0x72, 0x74, 0};
-/** UTF-8 encoded string from: 0.2.0 */
+// UTF-8 encoded string from: 0.2.0
 static const unsigned char widget_text_1[] = {0x30, 0x2e, 0x32, 0x2e, 0x30, 0};
-/** UTF-8 encoded string from: A%20minimal%20LCUI%20application */
+// UTF-8 encoded string from: A%20minimal%20LCUI%20application
 static const unsigned char widget_text_2[] = {0x41, 0x20, 0x6d, 0x69, 0x6e, 0x69, 0x6d, 0x61, 0x6c, 0x20, 0x4c, 0x43, 0x55, 0x49, 0x20, 0x61, 0x70, 0x70, 0x6c, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0};
-/** UTF-8 encoded string from: Visit%20homepage */
+// UTF-8 encoded string from: Visit%20homepage
 static const unsigned char widget_text_3[] = {0x56, 0x69, 0x73, 0x69, 0x74, 0x20, 0x68, 0x6f, 0x6d, 0x65, 0x70, 0x61, 0x67, 0x65, 0};
-/** UTF-8 encoded string from: %EE%83%AA */
+// UTF-8 encoded string from: %EE%83%AA
 static const unsigned char widget_text_4[] = {0xee, 0x83, 0xaa, 0};
-/** UTF-8 encoded string from: Report%20an%20issue */
+// UTF-8 encoded string from: Report%20an%20issue
 static const unsigned char widget_text_5[] = {0x52, 0x65, 0x70, 0x6f, 0x72, 0x74, 0x20, 0x61, 0x6e, 0x20, 0x69, 0x73, 0x73, 0x75, 0x65, 0};
-/** UTF-8 encoded string from: %EE%83%AA */
+// UTF-8 encoded string from: %EE%83%AA
 static const unsigned char widget_text_6[] = {0xee, 0x83, 0xaa, 0};
 
 static void about_load_template(ui_widget_t *parent, about_refs_t *refs)
